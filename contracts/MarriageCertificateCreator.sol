e217969e5d15c185920267099b279e6599582aa9
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2019-02-12 11:58:53 +0800

    Updated MarriageCertificate contract to allow deposit and withdrawal from joint account and savings account

--- contracts/MarriageCertificateCreator.sol
+++ contracts/MarriageCertificateCreator.sol
@@ -71,8 +71,17 @@ contract MarriageCertificate {
     bool[2] public isValid;
     address[2] public spousesAddresses;
     uint public timestamp;
+    mapping(bytes32 => uint) public accounts;
+    struct withdrawRequestFromSavings {
+        address payable sender;
+        uint amount;
+        uint timestamp;
+        bool approved;
+    }
+    mapping(uint => withdrawRequestFromSavings) withdrawRequests;
     
     event MarriageValidity(bool[2] validity);
+    event NewWithdrawalRequestFromSavings(uint request);
     
     /** 
         @dev function is called every time someone wants to register a new marriage certificate
@@ -95,6 +104,8 @@ contract MarriageCertificate {
         isValid = [true, false];
         spousesAddresses = [certificateCreator, spouse2address];
         timestamp = now;
+        accounts["joint"] = 0;
+        accounts["savings"] =  0;
     }
     
     /// @dev some functions can only be accessed by one of the two spouses
@@ -114,8 +125,8 @@ contract MarriageCertificate {
     }
     
     /// @dev only the two spouses have access to this function
-    function returnBalance() public view returns (uint) {
-        return address(this).balance;
+    function returnBalances() public view returns (uint, uint, uint) {
+        return (address(this).balance, accounts["joint"], accounts["savings"]);
     }
     
     /// @notice allows spouses to change contract state
@@ -130,18 +141,77 @@ contract MarriageCertificate {
     }
     
     /// @notice allows spouses or third parties to deposit money in marriage contract
-    function deposit(uint amount) public payable {
+    function deposit(uint amount, string memory account) public payable {
         // we check the amount sent is the amount required
         require(msg.value == amount, "Wrong amount sent!");
-    }
-    
-    /// @notice allows spouses to withdraw money from the account
-    function withdraw(uint amount) public onlySpouses {
-        if(amount < address(this).balance) {
-            msg.sender.transfer(amount);
+        // we update the balance according to the account type selected
+        if(keccak256(abi.encodePacked(account)) == keccak256(abi.encodePacked("joint"))) {
+            accounts["joint"] = accounts["joint"] + amount;
+        } else if(keccak256(abi.encodePacked(account)) == keccak256(abi.encodePacked("savings"))) {
+            accounts["savings"] = accounts["savings"] + amount;
         }
     }
     
-    /// @notice fallback function to send money directly
-    function() external payable {}
+    /// @notice allows spouses to withdraw money from the account
+    function withdraw(uint amount, string memory account) public onlySpouses {
+        // requested amount cannot exceed total balance of account
+        if(amount < address(this).balance) {
+            // we check if the balance is sufficient for the withdrawal from the joint account
+            if(keccak256(abi.encodePacked(account)) == keccak256(abi.encodePacked("joint")) && 
+                accounts["joint"] > amount) {
+                msg.sender.transfer(amount);
+            } else if(keccak256(abi.encodePacked(account)) == keccak256(abi.encodePacked("savings")) && 
+                accounts["savings"] > amount) {
+                // we create a request that the second spouse must approve in order to allow the transfer
+                withdrawRequestFromSavings memory newRequest = withdrawRequestFromSavings({
+                    sender: msg.sender,
+                    amount: amount,
+                    timestamp: now,
+                    approved: false
+                });
+                // we emit the new request with id number that will help find it in requests mapping
+                uint requestID = uint(keccak256(abi.encodePacked(msg.sender, amount, now)));
+                emit NewWithdrawalRequestFromSavings(requestID);
+                // we save the new request in requests mapping
+                withdrawRequests[requestID] = newRequest;
+            }
+        } else {
+            revert();
+        }
+    }
+    
+    /// @dev returns details about withdrawal request
+    function checkWithdrawRequest(uint requestID) public onlySpouses view returns (address, uint, uint, bool) {
+        withdrawRequestFromSavings memory request = withdrawRequests[requestID];
+        return (request.sender, request.amount, request.timestamp, request.approved);
+    }
+    
+    /** @notice allows spouse to accept withdrawal request from savings
+     *          and sends money to the account that created the request
+    **/
+    function approveWithdrawRequestFromSavings(uint requestID) public onlySpouses {
+        withdrawRequestFromSavings storage request = withdrawRequests[requestID];
+        // the request cannot have been approved before
+        if(request.approved == false) {
+            // the spouse approving the request cannot be the one who initiated it
+            if((spousesAddresses[0] == msg.sender && spousesAddresses[1] == request.sender) || 
+            (spousesAddresses[1] == msg.sender && spousesAddresses[0] == request.sender)) {
+                // mark the request as approved
+                request.approved = true;
+                // deduct amount from accounts mapping
+                accounts["savings"] = accounts["savings"] - request.amount;
+                // we transfer the money
+                request.sender.transfer(request.amount);
+            } else {
+            revert();
+            }
+        } else {
+            revert();
+        }
+    }
+    
+    /// @notice fallback function to send money directly, money stored in deposit account by default
+    function() external payable {
+        accounts["deposit"] = accounts["deposit"] + msg.value;
+    }
 }
